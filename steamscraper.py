f9042996d563eab12290ca6ae55e579bde036bbd
drcwadaniels <65857580+drcwadaniels@users.noreply.github.com>
2021-04-16 00:49:05 -0400

    Steam scraper now builds pandas dataframe, next is to write it to sql and have it run once an hour

--- steamscraper.py
+++ steamscraper.py
@@ -4,7 +4,10 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 from datetime import datetime
-#scrape the steam stats webpage
+from sqlalchemy import create_engine
+
+
+# scrape the steam stats webpage
 URL = "https://store.steampowered.com/stats/Steam-Game-and-Player-Statistics"
 steamstats = requests.get(URL)
 steamstats_soup = BeautifulSoup(steamstats.content, 'html.parser')
@@ -12,13 +15,13 @@ gamesstats = steamstats_soup.find(id='detailStats')
 games = gamesstats.find_all('a', class_='gameLink')
 people = gamesstats.find_all('span', class_='currentServers')
 
-#create list of top 100 games
+# create list of top 100 games
 
 game_list = list()
 for g in games:
     game_list.append(g.text)
 
-#zip people counts
+# zip people counts
 people_list = list()
 for p in people:
     people_list.append(int(int(p.text.replace(',', ''))))
@@ -26,7 +29,7 @@ people_iter = iter(people_list)
 people_game_counts = [*zip(people_iter, people_iter)]
 people_game_counts = np.array(people_game_counts)
 
-#grab datetime
+# grab datetime
 time = datetime.now()
 print(time)
 
@@ -37,7 +40,11 @@ game_database.Game = game_list
 game_database.CurrentPlayers = people_game_counts[:, 0]
 game_database.PeakPlayers = people_game_counts[:, 1]
 
-game_database.to_sql()
+#create engine for pandas sql
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root", pw="u$watchmenR15!", db = "steamraces"))
+
+game_database.to_sql('counts', con=engine, if_exists="append")
 
 
 
