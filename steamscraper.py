e505972fbca5169e2c4d5c710a2ba81c5b6a3eff
drcwadaniels <65857580+drcwadaniels@users.noreply.github.com>
2021-04-19 23:26:53 -0400

    Reverted to simpler version because what I was trying to implement was unnecessairily complicated. But kept the code because it was a nice way of handling SQL errors.

--- steamscraper.py
+++ steamscraper.py
@@ -1,4 +1,7 @@
 # This script is for scraping the steam stats website
+
+# -----------------------------Import these packages
+
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
@@ -9,16 +12,11 @@ import sqlalchemy
 from apscheduler.schedulers.background import BlockingScheduler
 import logging
 
+# ----------------------------This marks the beginning of the program
 
-def steamscraper(d, h):
-    h = h + 1  # increment hour
 
-    if h == 25:  # catch if in the next day (24 hour epoc)
-        h = 1
-        d = d + 1
-
+def steamscraper():  # webscraping function
     # scrape the steam stats webpage
-    print("Steam scraper for entry:", d, h)
     url = "https://store.steampowered.com/stats/Steam-Game-and-Player-Statistics"
     steamstats = requests.get(url)
     steamstats_soup = BeautifulSoup(steamstats.content, 'html.parser')
@@ -43,9 +41,7 @@ def steamscraper(d, h):
     # grab datetime
     time = datetime.now()
 
-    game_database = pd.DataFrame(columns={'Day', 'Hour', 'Date', 'Time', 'Game', 'CurrentPlayers', 'PeakPlayers'})
-    game_database.Day = np.repeat(d, 100)
-    game_database.Hour = np.repeat(h, 100)
+    game_database = pd.DataFrame(columns={'Date', 'Time', 'Game', 'CurrentPlayers', 'PeakPlayers'})
     game_database.Date = [time.strftime("%y/%m/%d")] * 100
     game_database.Time = [time.strftime("%H/%M/%S")] * 100
     game_database.Game = game_list
@@ -58,31 +54,6 @@ def steamscraper(d, h):
                                                                                db="steamraces"))
 
     game_database.to_sql('counts', con=engine, if_exists="append")
-    print("Steam scraper finished for entry:", d, h)
-
-
-# index entries to sql server
-def get_entries():
-    engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root",
-                                                                               pw="",db="steamraces"))
-    try:
-        entries = pd.read_sql('SELECT DISTINCT(Day) FROM steamraces.counts', con=engine)
-        d = entries.iloc[-1].to_numpy()
-    except sqlalchemy.exc.ProgrammingError as e:
-        print("No days")
-        d = 0
-
-    try:
-        entries = pd.read_sql('SELECT DISTINCT(Hour) FROM steamraces.counts', con=engine)
-        h = entries.iloc[-1].to_numpy()
-    except sqlalchemy.exc.ProgrammingError as e:
-        print("No hours")
-        h = 0
-    return d, h
-
-
-# get entries for indexing
-d, h = get_entries()
 
 
 # scheduler
@@ -90,16 +61,15 @@ if __name__ == '__main__':
     logging.basicConfig()
     logging.getLogger('apscheduler').setLevel(logging.DEBUG)
     scheduler = BlockingScheduler()
-    scheduler.add_job(lambda: steamscraper(d, h), 'interval', minutes=60, replace_existing=True)
-    if d == 0 and h == 0:
-        steamscraper(d, h)
+    scheduler.add_job(lambda: steamscraper(), 'interval', minutes=60, replace_existing=True)
+    steamscraper()
     scheduler.start()
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
     try:
         # keeping things alive, as we know it; and I feel fine!
         while True:
-            time.sleep(58)
+            time.sleep(59)
     except (KeyboardInterrupt, SystemExit):
         # kill this program
         scheduler.shutdown()
