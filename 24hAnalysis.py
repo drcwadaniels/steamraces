2dc7b5119c31958e7c3ad762473ba4b2003947dc
drcwadaniels <65857580+drcwadaniels@users.noreply.github.com>
2021-04-20 23:18:47 -0400

    24 hour analysis still being constructed, but thus far I have it generating figures that individuals can visually inspect. Next up will be describing the slopes or change points of these functions.

--- 24hAnalysis.py
+++ 24hAnalysis.py
@@ -1,18 +1,77 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from labellines import labelLine, labelLines
 from sqlalchemy import create_engine
-import pymysql
-import os
-from apscheduler.schedulers.background import BlockingScheduler
-import logging
-from datetime import datetime
-
+import sqlalchemy
 
 # establish engine for pulling data
-# engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root", pw="u$watchmenR15!", db="steamraces"))
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root", pw="",
+                                                                           db="steamraces"))
 
-# calculate range of data
-yesterday = datetime.now() - datetime.timedelta(days=1)
+# pull data with pandas
+tabbed_data = pd.read_sql("""SELECT * FROM steamraces.counts""", con=engine)
 
-print(yesterday)
+current_users_by_game = tabbed_data.groupby(['Game', 'Time']).sum() \
+    .groupby(level=0).cumsum().reset_index()
+min_current_users_by_game = current_users_by_game.groupby(['Game']).min().reset_index().sort_values(
+    by=['CurrentPlayers'], ascending=False)
+max_current_users_by_game = current_users_by_game.groupby(['Game']).max().reset_index().sort_values(
+    by=['CurrentPlayers'], ascending=False)
+
+
+i = 0
+i2 = 1
+label = []
+plt1 = plt.figure()
+for game in max_current_users_by_game.Game.unique():
+    label.append(game)
+    i += 1
+    if i == 21:
+        i = 1
+        i2 += 1
+    if i <= 10:
+        m = "o"
+    elif i == 11:
+        m = "*"
+    plt.subplot(2, 4, i2)
+    plt.plot(current_users_by_game.CurrentPlayers[current_users_by_game['Game'] == game].to_numpy()/
+             min_current_users_by_game.CurrentPlayers[min_current_users_by_game['Game'] == game].to_numpy(),
+             alpha=0.2, marker=m, label=game)
+    if i2 == 1 or i2 == 5:
+        plt.ylabel('Proportion of the Min Cum Sum of Current Players')
+    if i2 >= 5:
+        plt.xlabel('Ordinal Hour: Hour 0 = First Appearance in Top 100')
+    plt.legend(bbox_to_anchor=(1.7, 0.8, 0.3, 0.2), loc='upper right', prop={'size':7.5})
+plt.subplots_adjust(wspace=1.15)
+plt.tight_layout()
+plt.savefig("RelativePlayers.png")
+
+
+
+
+i = 0
+i2 = 1
+label = []
+plt2 = plt.figure()
+for game in max_current_users_by_game.Game.unique():
+    label.append(game)
+    i += 1
+    if i == 21:
+        i = 1
+        i2 += 1
+    if i <= 10:
+        m = "o"
+    elif i == 11:
+        m = "*"
+    plt.subplot(2, 4, i2)
+    plt.plot(current_users_by_game.CurrentPlayers[current_users_by_game['Game'] == game].to_numpy(),
+             alpha=0.2, marker=m, label=game)
+    if i2 == 1 or i2 == 5:
+        plt.ylabel('Cum Sum of Current Players')
+    if i2 >= 5:
+        plt.xlabel('Ordinal Hour: Hour 0 = First Appearance in Top 100')
+    plt.legend(bbox_to_anchor=(1.7, 0.8, 0.3, 0.2), loc='upper right', prop={'size':7.5})
+plt.subplots_adjust(wspace=1.15)
+plt.tight_layout()
+plt.savefig("AbsolutePlayers.png")
